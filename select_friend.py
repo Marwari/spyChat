5c38025e60c8063fbb434431365f93143fbe231e
Marwari <bharattitop@gmail.com>
2017-09-21 12:12:15 +0530

    select friend updated.

--- select_friend.py
+++ select_friend.py
@@ -19,6 +19,14 @@ def select_friend():
         counter = counter + 1
 
     # ask user which friend he wants to select to chat with
-    result = int(raw_input(colored("\nSelect from the list : ", 'magenta')))
-    # result the selected friend to perform action
-    return result - 1
+    friend_choice = int(raw_input(colored("\nSelect from the list : ", 'magenta')))
+    #  selected friend to perform action
+    friend_chosen = int(friend_choice) - 1
+
+    # Check if user has out of index choice.
+    if friend_chosen + 1> len(friends):
+        print (colored("Sorry, this friend is not present.", "green"))
+        exit()
+    else:
+        # returns the selected friend to perform actions
+        return friend_chosen
